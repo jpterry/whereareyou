669beae854dcd7bb2b7d5d63f40048e7d6db7bfb
John Terry <jpterry@gmail.com>
2012-08-18 12:05:26 -0700

    Added foundation framework

---
+++ public/javascripts/trace_client.js
@@ -0,0 +1,44 @@
+      var ss  = function(){
+        var self = this;
+        this.socket = null;
+
+        this.on_geo_error = function(eh){
+          console.log(eh.message);
+        }
+
+        this.on_geo_success = function(position){
+          console.log(position);
+          var msg = {
+            streamId: "xxx-yyy-zzz",
+            streamSig: "123123123123",
+            coords: position.coords
+          };
+
+          var msg_str = JSON.stringify(msg);
+          self.socket.send(msg_str);
+        }
+
+        function begin_trace(){
+          if(navigator.geolocation) {
+            navigator.geolocation.watchPosition(this.on_geo_success, this.on_geo_error);
+          } else {
+            console.log("No supporty");
+          }
+        }
+
+        this.init_socket = function(){
+          self.socket = new WebSocket("ws://192.168.0.112:8080/");
+          self.socket.onopen = function(){
+            console.log("Socket has been opened!");
+            begin_trace();
+          }
+
+          self.socket.onmessage = function(msg){
+            console.log(msg);
+          }
+        }
+
+        $(function(){
+          self.init_socket();
+        });
+      }();
